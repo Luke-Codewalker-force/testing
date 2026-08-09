ba73bc8b70faac1aa22d5f575b63e473b29771dd
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2026-08-09 19:26:02 +0200

    test rename

---
+++ tests/TC-05_api_create_contact_message.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect, request } from "@playwright/test";
+
+test.describe("API Tests - Contact Messages", () => {
+  test("TC-05: Should successfully submit contact message via POST /message/", async ({
+    request,
+  }) => {});
+});
