a18bbbf5e75b5415c6cf49787a5e3553ac39aa26
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2026-08-13 14:33:59 +0200

    add booking page

---
+++ pages/booking/booking.page.ts
@@ -0,0 +1,66 @@
+import { type Locator, type Page } from "@playwright/test";
+import { type BookingFormData } from "./types";
+
+export class BookingPage {
+  private readonly openBookingFormButton: Locator;
+  private readonly submitBookingButton: Locator;
+  private readonly bookingCard: Locator;
+  private readonly firstNameInput: Locator;
+  private readonly lastNameInput: Locator;
+  private readonly emailInput: Locator;
+  private readonly phoneInput: Locator;
+
+  constructor(private page: Page) {
+    this.openBookingFormButton = this.page.locator("#doReservation", {
+      hasText: "Reserve Now",
+    });
+    this.submitBookingButton = this.page.locator("button:not(#doReservation)", {
+      hasText: "Reserve Now",
+    });
+    this.bookingCard = this.page.locator(".card-body", {
+      has: this.page.getByRole("heading", { name: "Book This Room", level: 2 }),
+    });
+    this.firstNameInput = this.bookingCard.getByRole("textbox", {
+      name: "Firstname",
+    });
+    this.lastNameInput = this.bookingCard.getByRole("textbox", {
+      name: "Lastname",
+    });
+    this.emailInput = this.bookingCard.getByRole("textbox", {
+      name: "Email",
+    });
+    this.phoneInput = this.bookingCard.getByRole("textbox", {
+      name: "Phone",
+    });
+  }
+
+  async fillBookingForm({
+    firstName,
+    lastName,
+    email,
+    phone,
+  }: BookingFormData): Promise<void> {
+    await this.firstNameInput.fill(firstName);
+    await this.lastNameInput.fill(lastName);
+    await this.emailInput.fill(email);
+    await this.phoneInput.fill(phone);
+  }
+
+  async submitBookingForm(): Promise<void> {
+    await this.submitBookingButton.click();
+  }
+
+  async openBookingForm(): Promise<void> {
+    await this.openBookingFormButton.click();
+  }
+
+  get thisPage(): Page {
+    return this.page;
+  }
+
+  get bookingConfirmationMessage(): Locator {
+    return this.page
+      .locator(".card-body", { hasText: "Booking Confirmed" })
+      .locator("p");
+  }
+}
---
+++ pages/booking/types.ts
@@ -0,0 +1,6 @@
+export type BookingFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+};
