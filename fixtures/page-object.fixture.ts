a17ae2921808b01496b73eeecebcf5b7f0fbccde
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2026-08-13 14:34:22 +0200

    update fixtures

--- fixtures/page-object.fixture.ts
+++ fixtures/page-object.fixture.ts
@@ -1,13 +1,18 @@
 import { test as base } from "@playwright/test";
 import { HomePage } from "../pages/home/home.page";
+import { BookingPage } from "../pages/booking/booking.page";
 
 type Pages = {
   homePage: HomePage;
+  bookingPage: BookingPage;
 };
 
 export const test = base.extend<Pages>({
   homePage: async ({ page }, use) => {
     await use(new HomePage(page));
+  },
+  bookingPage: async ({ page }, use) => {
+    await use(new BookingPage(page));
   },
 });
 
