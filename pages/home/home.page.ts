35cf27cd518e4e0c09b581c80359e2a7e85a0d7c
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2026-08-13 14:35:17 +0200

    update home page

--- pages/home/home.page.ts
+++ pages/home/home.page.ts
@@ -1,54 +1,19 @@
-import { Locator, Page } from "@playwright/test";
-import { ContactFormData } from "./types";
+import { Page } from "@playwright/test";
+import { ContactFormComponent } from "./components/contact-form.component";
+import { CheckBookingAvailabilityFormComponent } from "./components/check-availability-form.component";
 
 export class HomePage {
-  private readonly contactSection: Locator;
-  private readonly contactNameInput: Locator;
-  private readonly contactEmailInput: Locator;
-  private readonly contactPhoneInput: Locator;
-  private readonly subjectInput: Locator;
-  private readonly descriptionInput: Locator;
-  private readonly submitButton: Locator;
+  public readonly contactFormComponent: ContactFormComponent;
+  public readonly bookingFormComponent: CheckBookingAvailabilityFormComponent;
 
   constructor(private page: Page) {
-    this.contactNameInput = page.getByTestId("ContactName");
-    this.contactEmailInput = page.getByTestId("ContactEmail");
-    this.contactPhoneInput = page.getByTestId("ContactPhone");
-    this.subjectInput = page.getByTestId("ContactSubject");
-    this.descriptionInput = page.getByTestId("ContactDescription");
-    this.submitButton = page.getByRole("button", { name: "Submit" });
-    this.contactSection = page.locator("#contact");
-  }
-
-  async fillContactForm({
-    name,
-    email,
-    phone,
-    subject,
-    description,
-  }: ContactFormData): Promise<void> {
-    await this.contactNameInput.fill(name);
-    await this.contactEmailInput.fill(email);
-    await this.contactPhoneInput.fill(phone);
-    await this.subjectInput.fill(subject);
-    await this.descriptionInput.fill(description);
-  }
-
-  async submitContactForm(): Promise<void> {
-    await this.submitButton.click();
+    this.contactFormComponent = new ContactFormComponent(this.page);
+    this.bookingFormComponent = new CheckBookingAvailabilityFormComponent(
+      this.page,
+    );
   }
 
   async goto(url: string = "/"): Promise<void> {
     await this.page.goto(url);
-  }
-
-  get contactFormSection(): Locator {
-    return this.contactSection;
-  }
-
-  get messageElements(): Locator {
-    return this.contactSection
-      .locator(".card-body", { hasText: "Thanks for getting in touch" })
-      .locator("h3, p");
   }
 }
