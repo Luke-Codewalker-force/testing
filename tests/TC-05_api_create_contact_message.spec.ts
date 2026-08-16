afaf06371348036999e1562d720a00e347b73646
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2026-08-16 14:15:26 +0200

    add actual content to the TC-05

--- tests/TC-05_api_create_contact_message.spec.ts
+++ tests/TC-05_api_create_contact_message.spec.ts
@@ -1,7 +1,27 @@
 import { test, expect, request } from "@playwright/test";
+import { ContactFactory } from "../factories/contact-factory";
 
 test.describe("API Tests - Contact Messages", () => {
-  test("TC-05: Should successfully submit contact message via POST /message/", async ({
+  test.only("TC-05: Should successfully submit contact message via POST /message/", async ({
     request,
-  }) => {});
+  }) => {
+    // Arrange
+    const payload = ContactFactory.createContactData();
+
+    // Act
+    const response = await request.post("api/message/", {
+      data: payload,
+    });
+
+    // Assert
+    expect(response.status()).toBe(200);
+    expect(response.headers()["content-type"]).toContain("application/json");
+
+    const responseBody = await response.json();
+    expect(responseBody).toEqual(
+      expect.objectContaining({
+        success: true,
+      }),
+    );
+  });
 });
