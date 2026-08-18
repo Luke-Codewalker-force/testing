961a459901cf564e22e2e581c62a0a6afb16ed4b
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2026-08-18 14:01:16 +0200

    api-client added

---
+++ api/auth-client.ts
@@ -0,0 +1,23 @@
+import { APIRequestContext, expect } from "@playwright/test";
+
+export class AuthClient {
+  constructor(private request: APIRequestContext) {}
+
+  async login(
+    username = process.env.ADMIN_LOGIN,
+    password = process.env.ADMIN_PASSWORD,
+  ) {
+    const response = await this.request.post("/api/auth/login", {
+      data: { username, password },
+    });
+
+    return response;
+  }
+
+  async getToken(username?: string, password?: string): Promise<string> {
+    const response = await this.login(username, password);
+    const responseBody = await response.json();
+
+    return responseBody.token;
+  }
+}
