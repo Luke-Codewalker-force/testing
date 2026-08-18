fa8b914d4afee430e1477947305eb07bc6798fe9
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2026-08-18 15:04:08 +0200

    playwright.config updated

---
+++ api/room.client.ts
@@ -0,0 +1,15 @@
+import { APIRequestContext } from "@playwright/test";
+
+export class RoomClient {
+  constructor(private request: APIRequestContext) {}
+
+  // getRooms();
+
+  // getRoomById(id);
+
+  // createRoom(data, token);
+
+  // deleteRoom(id, token);
+
+  // editRoom(id, data, token); - PUT
+}
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig, devices } from "@playwright/test";
-import { loadEnvFile } from "node:process";
+import dotenv from "dotenv";
+import path from "path";
 
-loadEnvFile("./.env");
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
 export default defineConfig({
   testDir: "./tests",
   /* Run tests in files in parallel */
