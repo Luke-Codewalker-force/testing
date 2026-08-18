1b33852f6980004af968190b652170ce7aac2c0c
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2026-08-18 14:02:01 +0200

    fixtures updated

--- fixtures/page-object.fixture.ts
+++ fixtures/page-object.fixture.ts
@@ -1,18 +1,26 @@
 import { test as base } from "@playwright/test";
 import { HomePage } from "../pages/home/home.page";
 import { BookingPage } from "../pages/booking/booking.page";
+import { AuthClient } from "../api/auth-client";
 
-type Pages = {
+type Fixtures = {
+  // pages
   homePage: HomePage;
   bookingPage: BookingPage;
+
+  // api
+  authClient: AuthClient;
 };
 
-export const test = base.extend<Pages>({
+export const test = base.extend<Fixtures>({
   homePage: async ({ page }, use) => {
     await use(new HomePage(page));
   },
   bookingPage: async ({ page }, use) => {
     await use(new BookingPage(page));
+  },
+  authClient: async ({ request }, use) => {
+    await use(new AuthClient(request));
   },
 });
 
