c088ff0cab0928018253f7a538e5f2421702ef6a
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2026-08-25 15:01:03 +0200

    SCRUM-12: implement room types

--- api/types.ts
+++ api/types.ts
@@ -2,3 +2,31 @@ export type AuthData = {
   username?: string;
   password?: string;
 };
+
+type RoomType = "Double" | "Family" | "Single" | "Suite" | "Twin";
+
+type RoomFeatures = "Radio" | "Refreshments" | "Safe" | "TV" | "Views" | "WiFi";
+
+type FeaturesObject = {
+  Radio: boolean;
+  Refreshments: boolean;
+  Safe: boolean;
+  TV: boolean;
+  Views: boolean;
+  WiFi: boolean;
+};
+
+export type EditRoomData = RoomData & {
+  featuresObject: FeaturesObject;
+  roomId: number;
+};
+
+export type RoomData = {
+  accessible: boolean;
+  description: string;
+  features: RoomFeatures[];
+  image: string;
+  roomName: string;
+  roomPrice: string;
+  type: RoomType;
+};
