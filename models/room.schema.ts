681e5f2f3e7173baefe2be553933b23e93ff42cf
Lukasz Kowalczyk <lukasz.kowalczyk.dev@gmail.com>
2026-08-30 15:14:05 +0200

    SCRUM-12: lint

--- models/room.schema.ts
+++ models/room.schema.ts
@@ -1,4 +1,4 @@
-import { z } from "zod"
+import { z } from "zod";
 
 export const RoomTypeSchema = z.enum([
   "Double",
@@ -6,7 +6,7 @@ export const RoomTypeSchema = z.enum([
   "Single",
   "Suite",
   "Twin",
-])
+]);
 
 export const RoomFeatureSchema = z.enum([
   "Radio",
@@ -15,7 +15,7 @@ export const RoomFeatureSchema = z.enum([
   "TV",
   "Views",
   "WiFi",
-])
+]);
 
 export const FeaturesObjectSchema = z.object({
   Radio: z.boolean(),
@@ -24,10 +24,10 @@ export const FeaturesObjectSchema = z.object({
   TV: z.boolean(),
   Views: z.boolean(),
   WiFi: z.boolean(),
-})
+});
 
 export const RoomDataSchema = z.object({
-  accessible: z.boolean(),
+  accessible: z.optional(z.boolean()),
   description: z.string(),
   features: z.array(RoomFeatureSchema),
   image: z.string(),
@@ -35,22 +35,22 @@ export const RoomDataSchema = z.object({
   roomPrice: z.number().int(),
   roomid: z.number().int(),
   type: RoomTypeSchema,
-})
+});
 
 export const EditRoomDataSchema = RoomDataSchema.extend({
   roomId: z.number().int().positive(),
   featuresObject: FeaturesObjectSchema,
-})
+});
 
 export const RoomsListResponseSchema = z.union([
   z.object({
     rooms: z.array(RoomDataSchema),
   }),
-])
+]);
 
-export type RoomType = z.infer<typeof RoomTypeSchema>
-export type RoomFeature = z.infer<typeof RoomFeatureSchema>
-export type FeaturesObject = z.infer<typeof FeaturesObjectSchema>
-export type RoomData = z.infer<typeof RoomDataSchema>
-export type EditRoomData = z.infer<typeof EditRoomDataSchema>
-export type RoomsListResponse = z.infer<typeof RoomsListResponseSchema>
+export type RoomType = z.infer<typeof RoomTypeSchema>;
+export type RoomFeature = z.infer<typeof RoomFeatureSchema>;
+export type FeaturesObject = z.infer<typeof FeaturesObjectSchema>;
+export type RoomData = z.infer<typeof RoomDataSchema>;
+export type EditRoomData = z.infer<typeof EditRoomDataSchema>;
+export type RoomsListResponse = z.infer<typeof RoomsListResponseSchema>;
